d0eabde091a6e7428088272f3d1b1a0f0ab4fa54
Seth Tisue <seth@tisue.net>
2011-09-09 21:11:44 -0400

    simplify code
    by making the primitives inner objects, now that the extensions API
    permits it (changes I made earlier today)

--- src/TestExtension.scala
+++ src/TestExtension.scala
@@ -4,66 +4,69 @@ import org.nlogo.api.{ DefaultClassManager, PrimitiveManager,
 
 // extension
 class TestExtension extends DefaultClassManager {
+
   val tester = new Tester
+
   def load(manager: PrimitiveManager) {
-    manager.addPrimitive("setup", new SetupCommand(tester))
-    manager.addPrimitive("add", new AddCommand(tester))
-    manager.addPrimitive("run", new RunCommand(tester))
-    manager.addPrimitive("clear", new ClearCommand(tester))
-    manager.addPrimitive("summary", new SummaryReporter(tester))
-    manager.addPrimitive("details", new DetailsReporter(tester))
+    manager.addPrimitive("setup", SetupCommand)
+    manager.addPrimitive("add", AddCommand)
+    manager.addPrimitive("run", RunCommand)
+    manager.addPrimitive("clear", ClearCommand)
+    manager.addPrimitive("summary", SummaryReporter)
+    manager.addPrimitive("details", DetailsReporter)
   }
-}
 
-// primitives
-class SetupCommand(tester: Tester) extends DefaultCommand {
-  override def getSyntax =
-    Syntax.commandSyntax(Array(Syntax.CommandTaskType))
-  def perform(args: Array[Argument], context: Context) {
-    tester.setup = args(0).getCommandTask
+  // primitives
+  object SetupCommand extends DefaultCommand {
+    override def getSyntax =
+      Syntax.commandSyntax(Array(Syntax.CommandTaskType))
+    def perform(args: Array[Argument], context: Context) {
+      tester.setup = args(0).getCommandTask
+    }
   }
-}
 
-class AddCommand(tester: Tester) extends DefaultCommand {
-  override def getSyntax =
-    Syntax.commandSyntax(
-      Array(Syntax.StringType, Syntax.CommandTaskType,
-            Syntax.ReporterTaskType, Syntax.WildcardType))
-  def perform(args: Array[Argument], context: Context) {
-    tester.add(
-      Test(args(0).getString,
-           args(1).getCommandTask,
-           args(2).getReporterTask,
-           args(3).get))
+  object AddCommand extends DefaultCommand {
+    override def getSyntax =
+      Syntax.commandSyntax(
+        Array(Syntax.StringType, Syntax.CommandTaskType,
+              Syntax.ReporterTaskType, Syntax.WildcardType))
+    def perform(args: Array[Argument], context: Context) {
+      tester.add(
+        Test(args(0).getString,
+             args(1).getCommandTask,
+             args(2).getReporterTask,
+             args(3).get))
+    }
   }
-}
 
-class RunCommand(tester: Tester) extends DefaultCommand {
-  override def getSyntax =
-    Syntax.commandSyntax()
-  def perform(args: Array[Argument], context: Context) {
-    tester.run(context)
+  object RunCommand extends DefaultCommand {
+    override def getSyntax =
+      Syntax.commandSyntax()
+    def perform(args: Array[Argument], context: Context) {
+      tester.run(context)
+    }
   }
-}
 
-class ClearCommand(tester: Tester) extends DefaultCommand {
-  override def getSyntax =
-    Syntax.commandSyntax()
-  def perform(args: Array[Argument], context: Context) {
-    tester.clear()
+  object ClearCommand extends DefaultCommand {
+    override def getSyntax =
+      Syntax.commandSyntax()
+    def perform(args: Array[Argument], context: Context) {
+      tester.clear()
+    }
   }
-}
 
-class SummaryReporter(tester: Tester) extends DefaultReporter {
-  override def getSyntax =
-    Syntax.reporterSyntax(Syntax.StringType)
-  def report(args: Array[Argument], context: Context) =
-    tester.results.summary
-}
+  object SummaryReporter extends DefaultReporter {
+    override def getSyntax =
+      Syntax.reporterSyntax(Syntax.StringType)
+    def report(args: Array[Argument], context: Context) =
+      tester.results.summary
+  }
 
-class DetailsReporter(tester: Tester) extends DefaultReporter {
-  override def getSyntax =
-    Syntax.reporterSyntax(Syntax.StringType)
-  def report(args: Array[Argument], context: Context) =
-    tester.results.details
+  object DetailsReporter extends DefaultReporter {
+    override def getSyntax =
+      Syntax.reporterSyntax(Syntax.StringType)
+    def report(args: Array[Argument], context: Context) =
+      tester.results.details
+  }
+
 }
